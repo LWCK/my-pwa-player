f09fde3b86f2622595609dd1406642520a75d5ee
LWCK <christophe.kromer@ylwane.com>
2022-08-18 16:45:42 -1000

    ajouter form add link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ const Footer: React.FC<any> = () => {
 
     return (
         <Container fluid className="Footer py-3">
+            <p>Si vous souhaitez ajouter des titres <br /> cliquez sur le <span><a href='https://airtable.com/shrtWpyUK1yXzEDWO' target='_blank'>LIEN</a></span></p>
             by <strong>iTAHITI LAB</strong>
         </Container>
     )
