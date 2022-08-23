8e2f9cc7387a196f97eaa70cb259ce9886f694f6
LWCK <christophe.kromer@ylwane.com>
2022-08-22 15:53:43 -1000

    road to homepage step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import './App.css';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { useCallback } from "react";
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const App: React.FC<any> = () => {
-  const particlesInit = useCallback(async (engine: any) => {
-    console.log(engine);
 
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container: any | undefined) => {
-    await console.log(container)
-  }, []);
   return (
-
-    <Container>
+    <Container fluid>
       <Row>
-        <Col>
-
+        <Col className='mobile d-flex align-items-center justify-content-center text-center font'>
+          <Button variant=''><Link to='/Player'>Acceder au player</Link></Button>
         </Col>
       </Row>
     </Container>
-
   );
 };
+
 export default App;
